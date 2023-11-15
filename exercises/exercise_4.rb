4cd0cbf9bc66f3628627fb66911c775b47bebb3c
lorraineC26 <lorraine.quinyee@gmail.com>
2023-11-15 14:56:55 -0500

    calc practice

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,50 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+# create 3 more stores:
+Surrey = Store.create(
+  name: "Surrey",
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+Whistler = Store.create(
+  name: "Surrey",
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+Yaletown = Store.create(
+  name: "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+# Using the where class method from Active Record, 
+# fetch (a collection of) only those stores that carry men's apparel. 
+# Assign the results to a variable @mens_stores.
+@mens_stores = Store.where(
+  mens_apparel: true
+)
+
+# debug: pretty print those stores
+# pp @mens_stores
+
+# Loop through each of these stores
+# output their name and annual revenue on each line.
+@mens_stores.each do |store|
+  puts "The store named #{store.name} has an annunal revenue #{store.annual_revenue}"
+end
+
+# Do another fetch but this time load stores that carry women's apparel 
+# and are generating less than $1M in annual revenue.
+@womens_stores_less_revenue = Store.where(womens_apparel: true)
+                                    .where("annual_revenue < ?", 1000000)
+
+# works same as above but using string-style
+# Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1_000_000)
+
+pp @womens_stores_less_revenue
--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,15 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+# Output the total revenue for the entire company (all stores), 
+# using Active Record's .sum calculation method.
+@total_revenue = Store.sum("annual_revenue")
+p @total_revenue
+
+# output the average annual revenue for all stores
+@avg_annual_revenue = Store.average("annual_revenue")
+p @avg_annual_revenue
+
+# Output the number of stores that are generating $1M or more in annual sales. 
+# Hint: Chain together where and size (or count) Active Record methods.
+puts Store.where("annual_revenue > ?", 1000000).count
