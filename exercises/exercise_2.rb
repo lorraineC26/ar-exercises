f5f3351fe5fb057de8ab6e6a20a47e08a4f4d495
lorraineC26 <lorraine.quinyee@gmail.com>
2023-11-15 14:19:55 -0500

    update 1st store's name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+# Load the first store(id=1); assign it to an instance variable @store1
+@store1 = Store.find(1)
+
+# Load the second
+@store2 = Store.find(2)
+
+# update @store1 instance in the db
+@store1.update(name: "Bokchoy's Store")
+puts @store1.name
